e1d5e7ae3249a7bbcf0d490054a1ceaca34b4eaf
GhostmanBY <nahuelromero2709@gmail.com>
2024-07-04 08:16:29 -0300

    Sistema de combate gui

--- Sistema_combatev2.py
+++ Sistema_combatev2.py
@@ -1,3 +1,4 @@
+#importacion de toda libreria necesaria
 import os 
 import random 
 import tkinter as tk
@@ -5,6 +6,7 @@ from customtkinter import *
 from tkinter import messagebox
 from tkinter import ttk
 
+#Importacion de funciones de otros archivos
 from Pokedex import serch_pokemon_num  
 from Estadisticas import pokemones 
 from Movimiento import seleccionar_movimiento, Tipo_movimiento  
@@ -12,11 +14,12 @@ from Movimiento import Potencia_de_movimientos, Precicion_de_movimiento
 from Tabla_de_tipos import Tipos_pokemons, tipos_movimientos  
 from Tabla_de_tipos import Eficacia
 
+#Funcion para definir cual ataque se ara
 def decicion_ataque(indice, Pokemon_J, Pokemon_R, labelB, labelJ):
     global PsA_Rival, PsA_Jugador
     if PsA_Rival <= 0 or PsA_Jugador <= 0:
         labelJ.place_forget()
-        if PsA_Rival <= 0:
+        if PsA_Rival <= 0: #one un mensaje o otro si gana el jugador o el bot
             labelB.configure(text="El jugador a ganado", font=("Arial", 14, "bold"), text_color="green")
             labelB.pack(padx=40, pady=10)
         else:
@@ -30,11 +33,12 @@ def decicion_ataque(indice, Pokemon_J, Pokemon_R, labelB, labelJ):
     movimiento_B = movimiento_bot(Pokemon_R)
 
     if pokemones[Pokemon_J]["vel"] > pokemones[Pokemon_R]["vel"]:
-        precicion = random.randint(0, 100)
-        if Precicion_de_movimiento[movimiento] >= precicion:
-            PsA_Rival -= Daño(movimiento, Pokemon_J, Pokemon_R)
+        precicion = random.randint(0, 100) #numero random 
+        #si el numero ramdom esta dentro del rango de la presicion del movimiento acierta si no falla
+        if Precicion_de_movimiento[movimiento] >= precicion: 
+            PsA_Rival -= Daño(movimiento, Pokemon_J, Pokemon_R) #para el jugador si acierta
         else:
-            messagebox.showinfo("Precisión", "El movimiento falló")
+            messagebox.showinfo("Precisión", "El movimiento falló") #para el jugador si falla
 
         precicion = random.randint(0, 100)
         if Precicion_de_movimiento[movimiento_B] >= precicion:
@@ -54,6 +58,7 @@ def decicion_ataque(indice, Pokemon_J, Pokemon_R, labelB, labelJ):
         else:
             messagebox.showinfo("Precisión", "El movimiento falló")
 
+    #llama a la funcion para actualizar los texto donde refleja la vida de los pokemons
     actualizar_ps(labelB, labelJ, Pokemon_R, Pokemon_J)
 
 def actualizar_ps(labelB, labelJ, Pokemon_R, Pokemon_J):
@@ -61,21 +66,25 @@ def actualizar_ps(labelB, labelJ, Pokemon_R, Pokemon_J):
     vida_rival = pokemones[Pokemon_R]["hp"]
     vida_jugador = pokemones[Pokemon_J]["hp"]
 
-    if labelB.winfo_exists():
+    #verifica que mientras que los valores sean mayores cero actualiza los valores
+    if labelB.winfo_exists(): 
         if PsA_Rival > 0:
             labelB.configure(text=f"{Pokemon_R}\nPS: {max(0, PsA_Rival)}/{vida_rival}", font=("Arial", 12, "bold"))
     if labelJ.winfo_exists():
         if PsA_Jugador > 0:
             labelJ.configure(text=f"{Pokemon_J}\nPS: {max(0, PsA_Jugador)}/{vida_jugador}", font=("Arial", 12, "bold"))
 
+#Elije de forma alatoria el movimiento del bot 
 def movimiento_bot(pokemon_bot):
     movimiento = [seleccionar_movimiento(pokemon_bot, 0), seleccionar_movimiento(pokemon_bot, 1)]
     seleccion = random.choice(movimiento)
     return seleccion
 
+#Calcula el daño con la formula de los juegos originales
 def Daño(movimiento, Pokemon_A, Pokemon_D):
     N = 1
 
+    #dependiendo del tipo del ataque elije las stats corespondientes
     if Tipo_movimiento(movimiento) == "Físico":
         A = pokemones[Pokemon_A]["atk"]
         D = pokemones[Pokemon_D]["def"]
@@ -84,35 +93,41 @@ def Daño(movimiento, Pokemon_A, Pokemon_D):
         D = pokemones[Pokemon_D]["defE"]
 
     P = Potencia_de_movimientos[movimiento]
-
+    
+    #si el tipo del ataque es igual al del pokemon o no se le asigna el bonus correspondiente
     if Tipos_pokemons[Pokemon_A] == tipos_movimientos[movimiento]:
         B = 1.5
     else:
         B = 1
 
+    #utiliza el tipo del movimiento para saber si el mismo tiene mas o menos eficacia 
     E = Eficacia(tipos_movimientos[movimiento], Tipos_pokemons[Pokemon_D])
 
-    V = random.randint(85, 100)
+    V = random.randint(85, 100) #valor ramdompara la cuenta
 
+    #calculo final
     Damage = 0.01 * B * E * V * (((0.2 * N + 1) * A * P) / (25 * D) + 2)
 
     return int(Damage)
 
 def Pelea(Pokemon):
+    #declaracion de variable globales para usar los mismo variables en todos lados
     global PsA_Rival, PsA_Jugador, tempjugador, tempbot
     global Pokemon_Rival
     global vida_jugador, vida_rival
-    #pokemon del rival
+    
+    #pokemon del rival (seleccion alatoria)
     num = [1, 4, 7]
     R = random.choice(num)
     Pokemon_Rival = serch_pokemon_num(R)
 
+    #vida actual y total de los pokemons
     PsA_Rival = pokemones[Pokemon_Rival]["hp"]  # rival(bot)
     PsA_Jugador = pokemones[Pokemon]["hp"]  # jugador
-
     vida_rival = pokemones[Pokemon_Rival]["hp"]
     vida_jugador = pokemones[Pokemon]["hp"]
 
+    #crea la ventana de batalla
     root = tk.Tk()
     root.title("Sistema de combate Pokémon")
     root.geometry("600x500")
@@ -156,4 +171,4 @@ def Pelea(Pokemon):
     root.mainloop()
 
 # Llamada a la función principal
-Pelea("Charmander")  # Puedes cambiar "Charmander" por el Pokémon que desees
+#Pelea("Charmander")  # Puedes cambiar "Charmander" por el Pokémon que desees
